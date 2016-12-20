6a90f126939f7d41df9df4b1240435ac32cebdf9
Nicholas Tran <2010435@crpwit1467smini.laclubs.com>
2016-12-20 07:01:18 -0800

    Added buttons and switches with code behind

--- Control Fun/ViewController.swift
+++ Control Fun/ViewController.swift
@@ -11,10 +11,17 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var numberField: UITextField!
+    @IBOutlet weak var sliderLabel: UILabel!
+    @IBOutlet weak var leftSwitch: UISwitch!
+    @IBOutlet weak var rightSwitch: UISwitch!
+    @IBOutlet weak var doSomethingButton: UIButton!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        sliderLabel.text = "50"
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,5 +38,52 @@ class ViewController: UIViewController {
         numberField.resignFirstResponder()
     }
     
+    @IBAction func onSliderChanged(_ sender: UISlider) {
+        sliderLabel.text = "\(lroundf(sender.value))"
+    }
+    
+    @IBAction func onSwitchChanged(_ sender: UISwitch) {
+        let setting = sender.isOn
+        leftSwitch.setOn(setting, animated: true)
+        rightSwitch.setOn(setting, animated: true)
+    }
+    
+    @IBAction func toggleControls(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 { // "Switches" is selected
+            leftSwitch.isHidden = false
+            rightSwitch.isHidden = false
+            doSomethingButton.isHidden = true
+        } else {
+            leftSwitch.isHidden = true
+            rightSwitch.isHidden = true
+            doSomethingButton.isHidden = false
+        }
+    }
+    
+    @IBAction func onButtonPressed(_ sender: UIButton) {
+        let controller = UIAlertController(title: "Are you sure?", message: nil, preferredStyle: .actionSheet)
+        
+        let yesAction = UIAlertAction(title: "Yes, I'm sure", style: .destructive, handler: {action in
+             let msg = self.nameField.text!.isEmpty
+                ?"You can breathe easy, everything went OK."
+                :"You can breathe easy, \(self.nameField.text),"+"everything went OK."
+             let controller2 = UIAlertController(title: "Something Was Done", message: msg, preferredStyle: .alert)
+             let cancelAcdtion = UIAlertAction(title: "Phew!", style: .cancel, handler: nil)
+             controller2.addAction(cancelAcdtion)
+             self.present(controller2, animated: true, completion: nil)
+            })
+        
+        let noAction = UIAlertAction(title: "No Way!", style: .cancel, handler: nil)
+        
+        controller.addAction(yesAction)
+        controller.addAction(noAction)
+        
+        if let ppc = controller.popoverPresentationController{
+            ppc.sourceView = sender
+            ppc.sourceRect = sender.bounds
+        }
+        
+        present(controller, animated: true, completion: nil)
+    }
 }
 
